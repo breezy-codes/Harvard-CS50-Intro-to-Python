4fd1f57ade5f8851bc761ca8f6f8cf826685077f
BreezyPhoebe <114720008+BreezyPhoebe@users.noreply.github.com>
2023-06-18 17:25:54 +1000

    update

--- calculator.py
+++ calculator.py
@@ -2,7 +2,7 @@
 # This program is for a simple calculator
 
 #Version 1
-print("Version 1")
+print("Version 1 - Starter Code")
 # Ask the user for two numbers
 x = input("What's x? ")
 y = input("What's y? ")
@@ -12,9 +12,57 @@ z = int(x) + int(y)
 print("The sum of x and y is:", z)
 
 #Version 2
-print("Version 2")
+print("Version 2 - Uses Integers")
 # Ask the user for two numbers and declare them as integers
 x = int(input("What's x? "))
 y = int(input("What's y? "))
 # Calculate the sum
 print("Print the sum of x and y", x+y)
+
+#Version 3
+print("Version 3 - Uses Floats")
+# Ask the user for two numbers and declare them as floats to allow decimal points
+x = float(input("What's x? "))
+y = float(input("What's y? "))
+# Calculate the sum
+print("Print the sum of x and y", x+y)
+
+#Version 4
+print("Version 4 - Rounds the number")
+# Ask the user for two numbers and declare them as floats to allow decimal points
+x = float(input("What's x? "))
+y = float(input("What's y? "))
+# rounds the sum of x and y to 2 decimal places
+z = round(x+y, 2)
+# Calculate the sum
+print("Print the sum of x and y", z)
+
+#Version 5
+print("Version 5 - Formats a large number with commas")
+# Ask the user for two numbers and declare them as floats to allow decimal points
+x = float(input("What's x? "))
+y = float(input("What's y? "))
+# rounds the sum of x and y to 2 decimal places
+z = round(x+y, 2)
+# Calculate the sum, and format it with commas
+print("Print the sum of x and y", f"{z:,}")
+
+#Version 6
+print("Version 6 - Division")
+# Ask the user for two numbers and declare them as floats to allow decimal points
+x = float(input("What's x? "))
+y = float(input("What's y? "))
+# Calculate the division of x and y and round it to 2 decimal places
+z = round(x / y, 2)
+# Print the value of z
+print("Print x divided by y", z)
+
+#Version 7
+print("Version 7 - Division done using f-string")
+# Ask the user for two numbers and declare them as floats to allow decimal points
+x = float(input("What's x? "))
+y = float(input("What's y? "))
+# Calculate the division of x and y
+z = x / y
+# Print the value of z to 2 decimal points
+print("Print x divided by y", f"{z:.2f}")
--- helloname.py
+++ helloname.py
@@ -4,21 +4,21 @@
 # This also goes over formatting for the name, as well as splitting up the inputs
 
 # Version 1
-print("Version 1")
+print("Version 1 - Starter Code")
 # Take the user input for their name
 name = input("What's your name? ")
 # print out Hello + The user's name input
 print("Hello, " + name)
 
 # Version 2
-print("Version 2")
+print("Version 2 - Uses Comma")
 # Take the user input for their name
 name = input("What's your name? ")
 # This version uses the print statement with a comma to output multiple strings
 print("Hello,", name, name)
 
 # Version 3
-print("Version 3")
+print("Version 3 - Uses End")
 # Take the user input for their name
 name = input("What's your name? ")
 # This version uses the end="" to remove the new line that is automatically added to the end of the print statement
@@ -26,21 +26,21 @@ print("Hello, ", end="")
 print(name)
 
 # Version 4
-print("Version 4")
+print("Version 4 - Uses Sep")
 # Take the user input for their name
 name = input("What's your name? ")
 # This version uses the sep="" to remove the space that is automatically added between the strings
 print("Hello,", name, sep='')
 
 # Version 5
-print("Version 5")
+print("Version 5 - Uses F-String")
 # Take the user input for their name
 name = input("What's your name? ")
 # This version uses the f-string to format the string, the modern way to do it
 print(f"Hello, {name}")
 
 # Version 6
-print("Version 6")
+print("Version 6 - Uses formatters")
 # Take the user input for their name
 name = input("What's your name? ")
 # Removes whitespace from the start and end of the string
@@ -53,7 +53,7 @@ name = name.title()
 print(f"Hello, {name}")
 
 # Version 7
-print("Version 7")
+print("Version 7 - Groups formatters")
 # Take the user input for their name
 name = input("What's your name? ")
 # Removes whitespace from the start and end of the string, then capitalizes the first letter of each word in the string
@@ -63,14 +63,14 @@ name = name.strip().title()
 print(f"Hello, {name}")
 
 # Version 8
-print("Version 8")
+print("Version 8 - Groups formatters to one line")
 # Take the user input for their name, and includes the formatting all into one line
 name = input("What's your name? ").strip().title()
 # This version uses the f-string to format the string, the modern way to do it
 print(f"Hello, {name}")
 
 # Version 9
-print("Version 9")
+print("Version 9 - Groups formatters to one line and splits words")
 # Take the user input for their full name, and includes the formatting all into one line
 name = input("What's your full name? ").strip().title()
 # Split the users name ino first name and last name
