7ddb236345ca7743c2a00be7f6a00558c0139894
BreezyPhoebe <114720008+BreezyPhoebe@users.noreply.github.com>
2023-06-18 17:10:12 +1000

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,8 +1,20 @@
 # Program Task Number 3
+# This program is for a simple calculator
 
+#Version 1
+print("Version 1")
+# Ask the user for two numbers
 x = input("What's x? ")
 y = input("What's y? ")
+# Convert x and y to integers and calculates the sum
+z = int(x) + int(y)
+# Print the sum
+print("The sum of x and y is:", z)
 
-z = x + y
-
-print(z)
+#Version 2
+print("Version 2")
+# Ask the user for two numbers and declare them as integers
+x = int(input("What's x? "))
+y = int(input("What's y? "))
+# Calculate the sum
+print("Print the sum of x and y", x+y)
